00b36e32160de6929f917eaddd48da49e7281d8c
kirito2056 <hajin_2056@naver.com>
2023-04-13 21:19:37 +0900

    lec_20230413

---
+++ Network/src/thread/Thread1Demo.java
@@ -0,0 +1,28 @@
+public class Thread1Demo {
+
+    public static void main(String args[]) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+
+        for ( int i = 0 ; i < 5 ; i++) {
+            System.out.println("안녕. ");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+}
+ class MyRunnable implements Runnable {
+
+    @Override
+     public void run() {
+        for ( int i = 0 ; i < 5 ; i++) {
+            System.out.println("잘가");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {}
+        }
+    }
+}
+
